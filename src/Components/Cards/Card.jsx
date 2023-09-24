48884e7837bf167c5c378af3a8129d95e37d91e7
unknown <rohanrifat43@gmail.com>
2023-09-24 23:02:22 +0600

    all card show in website

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -9,15 +9,15 @@ const Card = ({ card }) => {
   } = card;
   return (
     <div>
-      <div className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div  style={{backgroundColor:card_bg_color}} className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <div className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
           <img src={picture} alt="ui/ux review check" />
         </div>
         <div className="p-6">
-          <h4 className="block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          <h4 style={{backgroundColor:category_bg_color}} className="block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased ">
             {category}
           </h4>
-          <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+          <p style={{color:text_color}} className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
             {title}
           </p>
         </div>
