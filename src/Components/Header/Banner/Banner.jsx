b3e06867cda0de5cbe6d3b202fcfd8b31a1d7d31
unknown <rohanrifat43@gmail.com>
2023-09-26 23:08:18 +0600

    Done project

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -1,34 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
-const Banner = () => {
-  const [inputValue,setInputValue]=useState('')
-  const handleSearch=(e)=>{
-   setInputValue(e.target.value)
-   
-  }
-  const handleSearchBtn=()=>{
-    console.log(inputValue)
-  }
+const Banner = ({handleCardData}) => {
+  const cards = useLoaderData();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const value = e.target.text.value;
+    const data = cards.filter((sData) => sData.category === value);
+    handleCardData(data);
+  };
+  
+
   return (
     <div className="p-4">
       <div
-      className="hero h-[400px]"
-      style={{
-        backgroundImage:
-          "url(https://i.ibb.co/tPz3tK9/Rectangle-4281.png?fbclid=IwAR0-j_at9tPKdwksdhUy0DC1nXGiGeF7TiWR7Wb_yoXc_BB5O-ZZbTg8ZZg)",
-      }}
-    >
-      <div className="hero-overlay bg-[white] bg-opacity-80"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div>
-          <h1 className="mb-5 text-3xl text-black font-bold">I Grow By Helping People In Need</h1>
+        className="hero h-[400px]"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/tPz3tK9/Rectangle-4281.png?fbclid=IwAR0-j_at9tPKdwksdhUy0DC1nXGiGeF7TiWR7Wb_yoXc_BB5O-ZZbTg8ZZg)",
+        }}
+      >
+        <div className="hero-overlay bg-[white] bg-opacity-80"></div>
+        <div className="hero-content text-center text-neutral-content">
           <div>
-          <input value={inputValue} onChange={handleSearch}  id="searchField" type="text" placeholder="Type here" className="input text-black input-bordered w-full max-w-xs" />
-          <button onClick={handleSearchBtn} className="btn bg-[#FF444A]">Search</button>
+            <h1 className="mb-5 text-3xl text-black font-bold">
+              I Grow By Helping People In Need
+            </h1>
+            <form onSubmit={handleSearch}>
+              <input
+                type="text"
+                placeholder="Search Here"
+                name="text"
+                className="input input-bordered input-primary text-black w-full max-w-xs"
+              />
+              <input
+                className="bg-[#FF444A] p-[13px] rounded-lg text-base font-semibold text-white"
+                type="submit"
+                value="Search"
+              />
+            </form>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,24 @@
 import { useLoaderData } from "react-router-dom";
+import {useState,useEffect} from "react"
 import Cards from "../../Components/Cards/Cards";
 import Banner from "../../Components/Header/Banner/Banner";
 
 
 const Home = () => {
     const cards=useLoaderData();
+    const [cardsData,setCardsData]=useState(cards);
+    useEffect(()=>{
+        setCardsData(cards)
+    },[cards])
+    const handleCardData=(data)=>{
+        setCardsData(data)
+    }
     return (
         <div>
            <div className="bg-[white] bg-opacity-75">
-           <Banner></Banner>
+           <Banner handleCardData={handleCardData}></Banner>
            </div>
-           <Cards cards={cards}></Cards>
+           <Cards cards={cardsData}></Cards>
         </div>
     );
 };
